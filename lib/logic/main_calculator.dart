576e7d90f2a001e616c849ac043b0b81889d095b
cardinaldeacre <cardinaldeacre@gmail.com>
2026-05-15 21:29:27 +0700

    add new part

--- lib/logic/main_calculator.dart
+++ lib/logic/main_calculator.dart
@@ -1,6 +1,8 @@
 import 'package:waristmate_app/controllers/calculator_controller.dart';
 part 'calculate_special.dart';
 part 'calculate_masalah.dart';
+part 'calculate_kpk.dart';
+part 'calculate_ratio.dart';
 
 extension MainCalculator on CalculatorController {
   void startCounting() {
