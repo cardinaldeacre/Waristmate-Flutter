cb7eb3abb1730a28977b2fae00f7883d44870e78
cardinaldeacre <cardinaldeacre@gmail.com>
2026-05-18 21:42:48 +0700

    init aul & radd algorithm

---
+++ lib/logic/calculate_aul_radd.dart
@@ -0,0 +1,188 @@
+part of 'main_calculator.dart';
+
+extension CalculateAulRadd on CalculatorController {
+  void calculateAulRadd() {
+    if (totalAshobah > 0 && totalRatio <= hasilKPK) {
+      calcualteAshabah();
+    } else if (totalRatio > hasilKPK) {
+      // aul
+      hasilKPK = totalRatio;
+    } else if (totalRatio < hasilKPK && (nilaiSuami > 0 || nilaiIstri > 0)) {
+      // radd pasutri
+    } else if (totalRatio < hasilKPK && (nilaiSuami == 0 && nilaiIstri == 0)) {
+      // radd biasa
+      hasilKPK = totalRatio;
+
+      if (nilaiAyah > 0) {
+        if (nilaiAnakLaki > 0 || nilaiCucuLaki > 0) {
+          int bagianAyah = totalWarisan * rAyah ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Ayah',
+            'porsi': '$rAyah / $hasilKPK',
+            'nominal': bagianAyah,
+          });
+          sisaWarisan -= bagianAyah;
+        }
+      }
+
+      if (nilaiIbu > 0) {
+        if (keturunan > 0 || saudara > 1) {
+          int bagianIbu = totalWarisan * rIbu ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Ibu',
+            'porsi': '$rIbu / $hasilKPK',
+            'nominal': bagianIbu,
+          });
+          sisaWarisan -= bagianIbu;
+        } else {
+          int bagianIbu = totalWarisan * rIbu ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Ibu',
+            'porsi': '$rIbu / $hasilKPK',
+            'nominal': bagianIbu,
+          });
+          sisaWarisan -= bagianIbu;
+        }
+      }
+
+      if (nilaiKakek > 0) {
+        if (keturunanLaki > 0) {
+          int bagianKakek = totalWarisan * rKakek ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Kakek',
+            'porsi': '$rKakek / $hasilKPK',
+            'nominal': bagianKakek,
+          });
+          sisaWarisan -= bagianKakek;
+        }
+      }
+
+      if (nilaiNenekAyah > 0 && nilaiNenekIbu > 0) {
+        int bagianNenek2 = totalWarisan * rNenek2 ~/ hasilKPK ~/ nilaiNenek2;
+        results.add({
+          'ahli_waris': 'Nenek Ayah',
+          'porsi': '$rNenek2 / $hasilKPK Bersama',
+          'nominal': bagianNenek2,
+        });
+        results.add({
+          'ahli_waris': 'Nenek Ibu',
+          'porsi': '$rNenek2 / $hasilKPK Bersama',
+          'nominal': bagianNenek2,
+        });
+        sisaWarisan -= bagianNenek2;
+      } else if (nilaiNenekAyah > 0 && nilaiNenekIbu == 0) {
+        int bagianNenekAyah = totalWarisan * rNenekAyah ~/ hasilKPK;
+        results.add({
+          'ahli_waris': 'Nenek Ayah',
+          'porsi': '$rNenekAyah / $hasilKPK',
+          'nominal': bagianNenekAyah,
+        });
+        sisaWarisan -= bagianNenekAyah;
+      } else if (nilaiNenekIbu > 0 && nilaiNenekAyah == 0) {
+        int bagianNenekIbu = totalWarisan * rNenekIbu ~/ hasilKPK;
+        results.add({
+          'ahli_waris': 'Nenek Ibu',
+          'porsi': '$rNenekIbu / $hasilKPK',
+          'nominal': bagianNenekIbu,
+        });
+        sisaWarisan -= bagianNenekIbu;
+      }
+
+      if (nilaiAnakPerempuan > 0) {
+        if (nilaiAnakPerempuan > 1 && nilaiAnakLaki == 0) {
+          int bagianAnakPerempuan = totalWarisan * rAnakPerempuan ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Anak Perempuan',
+            'porsi': '$rAnakPerempuan / $hasilKPK',
+            'nominal': bagianAnakPerempuan,
+          });
+          sisaWarisan -= bagianAnakPerempuan;
+        } else if (nilaiAnakPerempuan == 1 && nilaiAnakLaki == 0) {
+          int bagianAnakPerempuan = totalWarisan * rAnakPerempuan ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Anak Perempuan',
+            'porsi': '$rAnakPerempuan / $hasilKPK',
+            'nominal': bagianAnakPerempuan,
+          });
+          sisaWarisan -= bagianAnakPerempuan;
+        }
+      }
+
+      if (nilaiCucuPerempuan > 0) {
+        if (nilaiAnakPerempuan == 1) {
+          int bagianCucuPerempuan = totalWarisan * rCucuPerempuan ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Cucu Perempuan',
+            'porsi': '$rCucuPerempuan / $hasilKPK',
+            'nominal': bagianCucuPerempuan,
+          });
+          sisaWarisan -= bagianCucuPerempuan;
+        } else if (nilaiCucuPerempuan == 1) {
+          int bagianCucuPerempuan = totalWarisan * rCucuPerempuan ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Cucu Perempuan',
+            'porsi': '$rCucuPerempuan / $hasilKPK',
+            'nominal': bagianCucuPerempuan,
+          });
+          sisaWarisan -= bagianCucuPerempuan;
+        } else if (nilaiCucuPerempuan > 1) {
+          int bagianCucuPerempuan = totalWarisan * rCucuPerempuan ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Cucu Perempuan',
+            'porsi': '$rCucuPerempuan / $hasilKPK',
+            'nominal': bagianCucuPerempuan,
+          });
+          sisaWarisan -= bagianCucuPerempuan;
+        }
+      }
+
+      if (nilaiSaudaraPerempuanKandung > 0) {
+        if (nilaiSaudaraPerempuanKandung == 1 &&
+            nilaiAnakPerempuan == 0 &&
+            nilaiCucuPerempuan == 0) {
+          int bagianSaudaraPerempuanKandung =
+              totalWarisan * rSaudaraPerempuanKandung ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Saudara Perempuan Kandung',
+            'porsi': '$rSaudaraPerempuanKandung / $hasilKPK',
+            'nominal': bagianSaudaraPerempuanKandung,
+          });
+          sisaWarisan -= bagianSaudaraPerempuanKandung;
+        } else if (nilaiSaudaraPerempuanKandung > 1 &&
+            nilaiAnakPerempuan == 0 &&
+            nilaiCucuPerempuan == 0) {
+          int bagianSaudaraPerempuanKandung =
+              totalWarisan * rSaudaraPerempuanKandung ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Saudara Perempuan Kandung',
+            'porsi': '$rSaudaraPerempuanKandung / $hasilKPK',
+            'nominal': bagianSaudaraPerempuanKandung,
+          });
+          sisaWarisan -= bagianSaudaraPerempuanKandung;
+        }
+      }
+
+      if (nilaiSaudaraPerempuanSeayah > 0) {
+        if (nilaiSaudaraPerempuanSeayah == 1) {
+          int bagianSaudaraPerempuanSeayah =
+              totalWarisan * rSaudaraPerempuanSeayah ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Saudara Perempuan Seayah',
+            'porsi': '$rSaudaraPerempuanSeayah / $hasilKPK',
+            'nominal': bagianSaudaraPerempuanSeayah,
+          });
+          sisaWarisan -= bagianSaudaraPerempuanSeayah;
+        } else if (nilaiSaudaraPerempuanSeayah > 1) {
+          int bagianSaudaraPerempuanSeayah =
+              totalWarisan * rSaudaraPerempuanSeayah ~/ hasilKPK;
+          results.add({
+            'ahli_waris': 'Saudara Perempuan Seayah',
+            'porsi': '$rSaudaraPerempuanSeayah / $hasilKPK',
+            'nominal': bagianSaudaraPerempuanSeayah,
+          });
+          sisaWarisan -= bagianSaudaraPerempuanSeayah;
+        }
+      }
+    }
+  }
+}
