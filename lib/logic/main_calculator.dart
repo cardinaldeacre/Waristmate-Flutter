e021323e706d11364fb6c4db721a40f33cf2492a
cardinaldeacre <cardinaldeacre@gmail.com>
2026-05-20 20:18:22 +0700

    import new function

--- lib/logic/main_calculator.dart
+++ lib/logic/main_calculator.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:waristmate_app/controllers/calculator_controller.dart';
 part 'calculate_special.dart';
 part 'calculate_masalah.dart';
 part 'calculate_kpk.dart';
 part 'calculate_ratio.dart';
+part 'calculate_ratio2.dart';
 part 'calculate_aul_radd.dart';
+part 'calculate_radd_pasutri.dart';
 part 'calculate_ashabah.dart';
 part 'calculate_share.dart';
 
