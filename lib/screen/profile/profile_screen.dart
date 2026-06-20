f61339d02204a39817772d797556ce30eee48ab6
cardinaldeacre <cardinaldeacre@gmail.com>
2026-06-20 21:10:18 +0700

    add preference and profile

--- lib/screen/profile/profile_screen.dart
+++ lib/screen/profile/profile_screen.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:waristmate_app/core/config/theme.dart';
+import 'package:waristmate_app/widgets/profile/profile_header.dart';
+import 'package:waristmate_app/widgets/profile/profile_card.dart';
+import 'package:waristmate_app/widgets/profile/preference_card.dart';
+import 'package:waristmate_app/widgets/profile/menu_card.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profile Screen')),
-      body: Center(child: Text('Welcome to the Profile Screen!')),
+      backgroundColor: AppColors.backgroundClean,
+      body: SafeArea(
+        child: Column(
+          children: [
+            ProfileHeader(),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    ProfileCard(),
+
+                    const SizedBox(height: 16),
+
+                    PreferenceCard(title: "Preferensi", child: Container()),
+
+                    const SizedBox(height: 16),
+
+                    MenuCard(
+                      title: "Riwayat Aktivitas",
+                      subtitle: "Terakhir diperbarui: 12 Juni 2024",
+                      iconData: Icons.history,
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    MenuCard(
+                      title: "Tentang Waristmate",
+                      subtitle: "Cari tahu lebih lanjut tentang aplikasi ini",
+                      iconData: Icons.info,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
