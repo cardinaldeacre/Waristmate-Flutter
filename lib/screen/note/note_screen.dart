2a1beca993e2f128367a86a9edb40a45b3355772
cardinaldeacre <cardinaldeacre@gmail.com>
2026-06-22 21:35:05 +0700

    init note screen

--- lib/screen/note/note_screen.dart
+++ lib/screen/note/note_screen.dart
@@ -1,13 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:waristmate_app/core/config/theme.dart';
+import 'package:waristmate_app/widgets/note/note_header.dart';
+import 'package:waristmate_app/widgets/note/note_card.dart';
 
-class NoteScreen extends StatelessWidget {
+class NoteScreen extends StatefulWidget {
   const NoteScreen({super.key});
+
+  @override
+  State<NoteScreen> createState() => _NoteScreenState();
+}
+
+class _NoteScreenState extends State<NoteScreen> {
+  bool _isEditMode = false;
+
+  void _toggleEditMode() {
+    setState(() {
+      _isEditMode = !_isEditMode;
+    });
+  }
+
+  void _saveData() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Catatan disimpan!'),
+        backgroundColor: AppColors.primaryGreen,
+      ),
+    );
+    _toggleEditMode();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Note Screen')),
-      body: Center(child: Text('Welcome to the Note Screen!')),
+      backgroundColor: AppColors.backgroundClean,
+      body: SafeArea(
+        child: Column(
+          children: [
+            NoteHeader(),
+            NoteCard(isEditMode: _isEditMode),
+
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: AppColors.cardBackground,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _isEditMode
+                        ? AppColors.primaryGreen
+                        : AppColors.gold,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: _isEditMode ? _saveData : _toggleEditMode,
+                  child: Text(
+                    _isEditMode ? 'Simpan Catatan' : 'Edit Catatan',
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textLight,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
