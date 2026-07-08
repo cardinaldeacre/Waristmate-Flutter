c287ee2b5ea10b46e00c6b502e81eba3ff5ed46d
cardinaldeacre <cardinaldeacre@gmail.com>
2026-07-08 13:38:24 +0700

    init build and version update

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+MONTHS=(
+  Januari Februari Maret April Mei Juni
+  Juli Agustus September Oktober November Desember
+)
+
+DAY=$(date +%-d)
+MONTH_INDEX=$(date +%-m)
+YEAR=$(date +%Y)
+
+MONTH=${MONTHS[$((MONTH_INDEX-1))]}
+
+BUILD_DATE="$DAY $MONTH $YEAR"
+
+echo "Build Date: $BUILD_DATE"
+
+cat > lib/core/config/app_info.dart <<EOF
+class AppInfo {
+  static const String buildDate = '$BUILD_DATE';
+}
+EOF
+
+flutter build apk
